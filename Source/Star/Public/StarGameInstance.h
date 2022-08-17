926aae306bfaf3237f1a09d22158755a86a81410
G1548798573 <111338339+G1548798573@users.noreply.github.com>
2022-08-17 13:11:02 +0800

    Add files via upload

---
+++ Source/Star/Public/StarGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "StarGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STAR_API UStarGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
